29d97457d3bdf98948d716201b809b30df934cf6
Giovanny Rodriguez <giovanny.rodriguez@imagunet.com>
2026-03-12 16:55:26 -0500

    chore: bump version to 1.3.0

--- glpi_utils/__init__.py
+++ glpi_utils/__init__.py
@@ -41,7 +41,7 @@ from .exceptions import (
 from .logger import EmptyHandler, SensitiveFilter
 from .version import GLPIVersion
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 
 __all__ = [
     # Clients
